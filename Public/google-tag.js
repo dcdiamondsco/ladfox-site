8e6624f27160b2db48ce11d3f2710659c77bc1f9
David <dcdiamondsco@gmail.com>
2026-04-03 14:08:20 +0100

    Centralize Google tag loader across site pages

---
+++ Public/google-tag.js
@@ -0,0 +1,14 @@
+(function () {
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=AW-17882921888';
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = window.gtag || function gtag() {
+    window.dataLayer.push(arguments);
+  };
+
+  window.gtag('js', new Date());
+  window.gtag('config', 'AW-17882921888');
+})();
