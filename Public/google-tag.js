b0efe044735dcd13056cca3dded0a9778d3b989b
David <dcdiamondsco@gmail.com>
2026-06-02 12:46:41 +0100

    Install direct Google tag snippet on live pages

--- Public/google-tag.js
+++ Public/google-tag.js
@@ -1,15 +1,6 @@
 (function () {
-  var script = document.createElement('script');
-  script.async = true;
-  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-V60YKFJX73';
-  document.head.appendChild(script);
-
   window.dataLayer = window.dataLayer || [];
   window.gtag = window.gtag || function gtag() {
     window.dataLayer.push(arguments);
   };
-
-  window.gtag('js', new Date());
-  window.gtag('config', 'G-V60YKFJX73');
-  window.gtag('config', 'AW-18205278360');
 })();
